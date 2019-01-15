adc03a94226abc057baa1e389bb4620674434e4f
haryp2309 <hary2309@gmail.com>
2019-01-15 14:05:51 +0100

    go

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 // JavaScript Document
 
-document.onscroll = function() {headertransition()}
 function headertransition() {
 var header = document.getElementById('header')
 header.setAttribute("class", "headertransition")
 }
 
-//look
-// Initial state
-//var scrollPos = 0;
-// adding scroll event
-//window.addEventListener('scroll', function(){
-  // detects new state and compares it with the new one
-//  if ((document.body.getBoundingClientRect()).top > scrollPos)
-//		document.getElementById('info-box').setAttribute('data-scroll-direction', 'UP');
-//	else
-//		document.getElementById('info-box').setAttribute('data-scroll-direction', 'DOWN');
-	// saves the new position for iteration.
-//	scrollPos = (document.body.getBoundingClientRect()).top;
-//});
+function rmheadertransition() {
+var header = document.getElementById('header')
+header.removeAttribute('class', 'headertransition')	
+}
+
+window.addEventListener('scroll', function(){
+if (document.body.getBoundingClientRect().y == 0){rmheadertransition()}else{headertransition();}
+})
