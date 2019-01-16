7abb5d53a5d986097222bd7a43d87d5daf7fa8e0
haryp2309 <hary2309@gmail.com>
2019-01-16 10:27:52 +0100

    go

--- script.js
+++ script.js
@@ -2,14 +2,29 @@
 
 function headertransition() {
 var header = document.getElementById('header')
-header.setAttribute("class", "headertransition")
+header.setAttribute("class", "headertransition");
+document.getElementById('sign-up').setAttribute("class", 'sign-up-transition')
 }
 
 function rmheadertransition() {
 var header = document.getElementById('header')
-header.removeAttribute('class', 'headertransition')	
+header.removeAttribute('class', 'headertransition');
+document.getElementById('sign-up').removeAttribute("class", 'sign-up-transition');
 }
 
 document.body.addEventListener('scroll', function(){
-if (document.getElementById('container-1').getBoundingClientRect().y == 0){rmheadertransition()}else{headertransition();}
+if (document.getElementById('container-1').getBoundingClientRect().y > -210){rmheadertransition(); flexFont();}else{headertransition();flexFont();}
 })
+
+// imported
+flexFont = function () {
+    var divs = document.getElementsByClassName("flexFont");
+    for(var i = 0; i < divs.length; i++) {
+        var relFontsize = divs[i].offsetHeight*0.30;
+        divs[i].style.fontSize = relFontsize+'px';
+    }
+};
+
+window.onload = function(event) {
+    flexFont();
+};
