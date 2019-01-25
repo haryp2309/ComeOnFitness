43cd1273b95d42b61a93ee9a4e2bb12b5fff94f7
haryp2309 <hary2309@gmail.com>
2019-01-25 06:49:33 +0100

    go

--- script.js
+++ script.js
@@ -4,33 +4,45 @@ function headertransition() {
 var header = document.getElementById('header');
 header.setAttribute("class", "headertransition");
 document.getElementById('sign-up').setAttribute("class", 'sign-up-transition');
-var header = document.getElementById('headertitle')
+var headertitle = document.getElementById('headertitle')
 headertitle.setAttribute("class", "headertransition");
+	console.log('headertransition runned');
 }
 
 function rmheadertransition() {
 var header = document.getElementById('header')
 header.removeAttribute('class', 'headertransition');
 document.getElementById('sign-up').removeAttribute("class", 'sign-up-transition');
-var header = document.getElementById('headertitle')
+var headertitle = document.getElementById('headertitle')
 headertitle.removeAttribute("class", "headertransition");
+	console.log('rmheadertransition runned');
+}
+
+function headerResize() {
+	var actualInnerWidth = document.body.clientWidth;
+	var header = document.getElementById('header');
+	header.style.width=actualInnerWidth+'px';
+	//console.log('headerResize runned');
 }
 
 document.body.addEventListener('scroll', function(){
-if (document.getElementById('container-1').getBoundingClientRect().y > -210){rmheadertransition(); flexFont();}else{headertransition();flexFont();}
+if (document.getElementById('container-1').getBoundingClientRect().y > -210){rmheadertransition();}else{headertransition();console.log('scrolled')}
 })
 
-flexFont = function () {
+function flexFont() {
     var divs = document.getElementsByClassName("flexFont");
     for(var i = 0; i < divs.length; i++) {
-        var relFontsize = divs[i].offsetHeight*0.028;
-        divs[i].style.fontSize = relFontsize+'vw';
+        var relFontsize = divs[i].offsetHeight;
+        divs[i].style.fontSize = relFontsize+'px';
+		console.log('flexFont runned');
     }
 };
 
 window.onload = function(event) {
-    flexFont();
+//    flexFont();
+	headerResize();
 };
+
 
 var c = document.getElementById("scrollArrow");
 var ctx = c.getContext("2d");
@@ -45,7 +57,3 @@ ctx.lineTo(700, 120);
 ctx.strokeStyle = "white";
 ctx.lineWidth = 8;
 ctx.stroke();
-
-var actualInnerWidth = document.body.clientWidth;
-var header = document.getElementById('header');
-header.style.width=actualInnerWidth+'px';
